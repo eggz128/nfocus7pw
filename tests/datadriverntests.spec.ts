657ca84ad0185a623fe8b211df3733e3de15a793
Steve Powell <steve.powell@edgewords.co.uk>
2024-12-05 12:36:41 +0000

    Added data and dynamic creation of data driven tests

---
+++ tests/datadriverntests.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+import { HomePOM } from './POMClasses/HomePOM';
+import { LoginPOM } from './POMClasses/LoginPOM';
+import logins from './data/logins.json' //Only valid in TS
+
+
+for(let credentials of logins){
+
+    test(`Data driven for username ${credentials.username}`, async ({page})=>{
+        await page.goto('https://www.edgewordstraining.co.uk/webdriver2/');
+      
+        const home = new HomePOM(page);
+        await home.goLogin();
+      
+        const loginpage = new LoginPOM(page);
+        
+        await loginpage.doLogin(credentials.username, credentials.password);
+      });
+}
+
