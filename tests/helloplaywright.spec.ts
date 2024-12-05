13de04c0a503c3981c5b80abaef2d5df838d8713
Steve Powell <steve.powell@edgewords.co.uk>
2024-12-05 08:18:13 +0000

    Fix tpo in HelloPlaywright sript

--- tests/helloplaywright.spec.ts
+++ tests/helloplaywright.spec.ts
@@ -21,7 +21,7 @@ test('My First Test', async ({page})=>{
     await page.goto("http://www.google.com/");
     //await page.getByRole('button', { name: 'Accept all' }).click(); //Not there when running on CI?
     await page.getByLabel('Search', { exact: true }).click();
-    await page.getByLabel('Search', { exact: true }).fill('edgwords');
+    await page.getByLabel('Search', { exact: true }).fill('edgewords');
     await page.getByLabel('Google Search').first().click();
     await expect(page.locator('#rso')).toContainText('Edgewords Training - Automated Software Testing Training');
     //await page.close();
